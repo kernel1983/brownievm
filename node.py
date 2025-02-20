456204622f8b41da7372870960bcdef36a8acce2
KJ <kernel1983@gmail.com>
2025-02-20 17:24:24 -0500

    add TESTNET_GENESIS_CONFIGURATION

--- node.py
+++ node.py
@@ -55,11 +55,11 @@ description: ethereum.genesis.GenesisFork[
     hex_to_address=ethereum.frontier.utils.hexadecimal.hex_to_address,
 )
 
-MAINNET_GENESIS_CONFIGURATION = ethereum.genesis.get_genesis_configuration("mainnet.json")
+TESTNET_GENESIS_CONFIGURATION = ethereum.genesis.get_genesis_configuration("testnet.json")
 
 state = ethereum.frontier.state.State()
-chain = ethereum.frontier.fork.BlockChain([], state, ethereum_types.numeric.U64(1))
-ethereum.genesis.add_genesis_block(description, chain, MAINNET_GENESIS_CONFIGURATION)
+chain = ethereum.frontier.fork.BlockChain([], state, ethereum_types.numeric.U64(31337))
+ethereum.genesis.add_genesis_block(description, chain, TESTNET_GENESIS_CONFIGURATION)
 
 class EthRpcHandler(tornado.web.RequestHandler):
     # def options(self):
@@ -73,25 +73,22 @@ class EthRpcHandler(tornado.web.RequestHandler):
 
     def post(self):
         # print(self.request.arguments)
-        self.add_header('access-control-allow-methods', 'OPTIONS, POST')
-        self.add_header('access-control-allow-origin', '*')
+        # self.add_header('access-control-allow-methods', 'OPTIONS, POST')
+        # self.add_header('access-control-allow-origin', '*')
         req = tornado.escape.json_decode(self.request.body)
         print(req)
         rpc_id = req.get('id', '0')
         if req.get('method') == 'eth_blockNumber':
-            # highest_block_height, _highest_block_hash, _highest_block = chain.get_highest_block() # change to get block number
             latest_block_height = chain.blocks[-1].header.number
             print(latest_block_height)
             resp = {'jsonrpc':'2.0', 'result': hex(latest_block_height), 'id':rpc_id}
 
         elif req.get('method') == 'eth_getBlockByNumber':
-            # latest_block_height = chain.get_latest_block_number()
             latest_block_height = chain.blocks[-1].header.number
             latest_block_hashes = [chain.blocks[-1].header.ommers_hash.hex()]
             print(latest_block_height, latest_block_hashes)
             if not latest_block_hashes:
                 latest_block_hashes.append('0'*64)
-            # resp = {'jsonrpc':'2.0', 'result': '0x'+highest_block_hash.decode('utf8'), 'id':rpc_id}
             resp = {"jsonrpc":"2.0", "id": rpc_id,
                 "result":{
                     # "number":"0x1",
@@ -126,37 +123,20 @@ class EthRpcHandler(tornado.web.RequestHandler):
             account = state._main_trie._data[web3.Web3.to_bytes(hexstr=address)]
             print(account)
             balance = int(account.balance)
-
-            # latest_block_height = chain.get_latest_block_number()
-            # db = database.get_conn()
-            # it = db.iteritems()
-            # it.seek(('globalstate_%s_' % address).encode('utf8'))
-            # balance = 0
-            # for subchain_key, subchain_value in it:
-            #     if not subchain_key.startswith(('globalstate_%s_' % address).encode('utf8')):
-            #         break
-            #     print('count subchainkey', subchain_key, subchain_value)
-
             resp = {'jsonrpc':'2.0', 'result': hex(balance), 'id':rpc_id}
 
         elif req.get('method') == 'eth_getTransactionReceipt':
             tx_hash = req['params'][0]
-            db = database.get_conn()
+            # db = database.get_conn()
             print(tx_hash)
-            tx_bytes = db.get(b'tx_%s' % tx_hash.encode('utf8'))
-            print(tx_bytes)
-            sender = tx_bytes.decode('utf8').split('_')[1]
-            subchain_block_json = db.get(tx_bytes)
-            print(subchain_block_json)
-            subchain_block = tornado.escape.json_decode(subchain_block_json)
-            data = subchain_block[chain.MSG_DATA]
-            print(data)
-            # count = data[0]
-            #signature = msg[chain.MSG_SIGNATURE]
-            #eth_tx_hash = eth_tx.hash_of_eth_tx_list(data)
-            #signature_obj = eth_account.Account._keys.Signature(bytes.fromhex(signature[2:]))
-            #pubkey = signature_obj.recover_public_key_from_msg_hash(eth_tx_hash)
-            #sender = pubkey.to_checksum_address()
+            # tx_bytes = db.get(b'tx_%s' % tx_hash.encode('utf8'))
+            # print(tx_bytes)
+            # sender = tx_bytes.decode('utf8').split('_')[1]
+            # subchain_block_json = db.get(tx_bytes)
+            # print(subchain_block_json)
+            # subchain_block = tornado.escape.json_decode(subchain_block_json)
+            # data = subchain_block[chain.MSG_DATA]
+            # print(data)
 
             result = {
                 'transactionHash': tx_hash,
@@ -179,7 +159,7 @@ class EthRpcHandler(tornado.web.RequestHandler):
         elif req.get('method') == 'eth_gasPrice':
             resp = {'jsonrpc':'2.0', 'result': '0x0', 'id': rpc_id}
 
-        elif req.get('method') == 'eth_estimateGas': # Fuck! Dont change this 0x5208 again!
+        elif req.get('method') == 'eth_estimateGas':
             resp = {'jsonrpc':'2.0', 'result': '0x5208', 'id': rpc_id} # 21000 gas
 
         elif req.get('method') == 'eth_maxPriorityFeePerGas':
@@ -190,19 +170,12 @@ class EthRpcHandler(tornado.web.RequestHandler):
             print('eth_getTransactionCount address', address)
             count = 0
 
-            # it = db.iteritems()
-            # it.seek(('subchain_%s_' % address).encode('utf8'))
-            # for subchain_key, subchain_value in it:
-            #     print('count subchainkey', subchain_key, subchain_value)
-            #     if subchain_key.decode('utf8').startswith('subchain_%s_' % address):
-            #         subchain_key_list = subchain_key.decode('utf8').split('_')
-            #         reversed_height = int(subchain_key_list[2])
-            #         count = setting.REVERSED_NO - reversed_height
-            #     break
-
             resp = {'jsonrpc':'2.0', 'result': hex(count), 'id': rpc_id}
 
         elif req.get('method') == 'eth_getBlockByHash':
+            resp = {'jsonrpc':'2.0', 'result': '0x0', 'id': rpc_id}
+
+        elif req.get('method') == 'eth_sendTransaction':
             resp = {'jsonrpc':'2.0', 'result': '0x0', 'id': rpc_id}
 
         elif req.get('method') == 'eth_sendRawTransaction':
@@ -421,7 +394,18 @@ class EthRpcHandler(tornado.web.RequestHandler):
             resp = {'jsonrpc':'2.0', 'result': str(setting.CHAIN_ID),'id': rpc_id}
 
         elif req.get('method') == 'eth_accounts':
-            resp = {'jsonrpc':'2.0', 'result': ['0x000d836201318ec6899a67540690382780743280'], 'id': rpc_id}
+            resp = {'jsonrpc':'2.0', 'result': [
+                    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+                    '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+                    '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+                    '0x90F79bf6EB2c4f870365E785982E1f101E93b906',
+                    '0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65',
+                    '0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc',
+                    '0x976EA74026E726554dB657fA54763abd0C3a0aa9',
+                    '0x14dC79964da2C08b23698B3D3cc7Ca32193d9955',
+                    '0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f',
+                    '0xa0Ee7A142d267C1f36714E4a8F75612F20a79720'
+                ], 'id': rpc_id}
 
         # print(resp)
         self.write(tornado.escape.json_encode(resp))
